04d303dfb18d2db619a4d405b7cdf1e829f3e485
zion0425 <siokim001@gmail.com>
2025-02-22 14:42:12 +0900

    chore: backend

---
+++ backend/ssage/ssage/src/main/java/com/ssagessage/ssage/SsageApplication.java
@@ -0,0 +1,13 @@
+package com.ssagessage.ssage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsageApplication.class, args);
+	}
+
+}
---
+++ backend/ssage/ssage/src/test/java/com/ssagessage/ssage/SsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssagessage.ssage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
