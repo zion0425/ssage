a456831eae7630c9419a959200b6088d7842944b
zion0425 <siokim001@gmail.com>
2025-02-22 20:09:07 +0900

    test

--- backend/ssage/src/main/java/com/ssage/controller/TestController.java
+++ backend/ssage/src/main/java/com/ssage/controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-public class Controller {
+public class TestController {
 
     @GetMapping("/test")
     public String testEndpoint() {
